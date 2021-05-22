c5990aac07257fd39282cede303097e1a348f910
Your NameSajRashid <sajid.rashid@live.co.uk>
2021-05-23 00:04:34 +0100

    updated login page validation

--- src/Client/Pages/Login/Login.razor.cs
+++ src/Client/Pages/Login/Login.razor.cs
@@ -16,21 +16,37 @@ namespace Chameleon.Client.Pages.Login
         private bool isCredsValid { get; set; } = false;
         InputText userNameReference;
         public Chameleon.Shared.Login UserCreds { get; set; } = new Chameleon.Shared.Login();
+        public string TxtType = "password";
 
-         protected override async Task OnAfterRenderAsync(bool first)
+        protected override async Task OnAfterRenderAsync(bool first)
          {
             if (first)
             {
                 await userNameReference.Element.Value.FocusAsync();
             }
          }
+        public void ShowPassword()
+        {
 
+            if (this.TxtType == "password")
+            {
+                this.TxtType = "text";
+            }
+            else
+            {
+                this.TxtType = "password";
+            }
+        }
         public async void HandleValidSubmit()
         {
             var password = UserCreds.Password;
+            var userName = UserCreds.Username;
+            if (userName.Length > 5 && password.Length > 7 )
+            {
+                isCredsValid = await Http.GetFromJsonAsync<bool>("api/login/" + UserCreds.Username + "?password=" + password);
+            }
             // generate a 128-bit salt using a secure PRNG
             //save password hash in DB
-            isCredsValid = await Http.GetFromJsonAsync<bool>("api/login/" + UserCreds.Username + "?password="+password);
         }
     }
 }
